26ef71e97615bae088d74b59fbd41a68345c43f3
sarmed <sarmed@Ubuntu-22.04.user.virtualbox.org>
2023-11-09 20:46:17 -0500

    modified existing script will implement new functions next, removed sentiemnt analysis for testing

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,7 +1,7 @@
 
 from zipfile import ZipFile
 import csv
-from transformers import pipeline
+import pandas as pd
 
 #Opens dataset reads data into array and removes therapist conversations
 def open_file(file_name):
@@ -25,29 +25,7 @@ def w_count(x):
   print(wcount)
   return wcount
 
-#Uses Hugging face api to perform sentiment analysis
-def sent_model(x):
-  sentiment_pipeline = pipeline("sentiment-analysis", model = "cardiffnlp/twitter-roberta-base-sentiment-latest")
-  data  = x
-  final_sentiment = sentiment_pipeline(data)
-  final_score = [0, 0, 0]
-  print(final_score)
-  for text in final_sentiment:
-    print(text)
-    if "positive" in text["label"]:
-      final_score[2] = 1 + final_score[2]
-    elif "negative" in text["label"]:
-      final_score[0] = final_score[0] + 1
-    else:
-      final_score[1] = final_score[1] + 1
-  print(final_score)
-  avg_score = ((1 * final_score[0]) + (2*final_score[1]) + (3*final_score[2]))/(final_score[0]+ final_score[1]+final_score[2])
-  avg_sentiment = round(avg_score)
-  print(avg_sentiment)
-  return avg_sentiment
-
-
-file_name = "/content/DataSets.zip"
+file_name = "content/DataSets.zip"
 
 with ZipFile(file_name, 'r') as zip:
   zip.extractall()
@@ -60,16 +38,15 @@ Sent = [0] * 258
 Length = [0] * 258
 count = 0
 #Opens csv file read filenames from file path may need to be changed
-with open('/content/DataSets/labeleddata/testlabels.csv', newline='') as csvfile:
+with open('DataSets/labeleddata/testlabels.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
       if(row["id"] == 'high_022' or row["id"] == 'low_023'):
         continue
-      x = open_file("/content/DataSets/"+row["id"])
+      x = open_file("DataSets/"+row["id"])
       id[count] = row["id"]
       label[count] = row["label"]
       Length[count] = w_count(x)
-      Sent[count] = sent_model(x)
       count = count + 1
 
 #Opens empty csv and writes labeled data
