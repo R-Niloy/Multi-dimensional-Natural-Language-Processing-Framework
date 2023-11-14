3b1ff7c130150c87d5743df5ceca0714a076c2b4
sarmed <sarmed@Ubuntu-22.04.user.virtualbox.org>
2023-11-13 23:05:38 -0500

    added turn counter as feature

--- feature_extraction.py
+++ feature_extraction.py
@@ -59,6 +59,28 @@ def w_countC(y):
   print(f"Client Words/Turn: {c_wcount}")
   return c_wcount
 
+#Returns turns taken by therapist
+def T_turns(y):
+  t_sentences = 0
+
+  for sentence in y:
+    if sentence[0] == 'T':
+      t_sentences+=1
+
+  print(f"t_sentences: {t_sentences}")
+  return t_sentences
+
+#Returns turns taken by Client
+def C_turns(y):
+  c_sentences = 0
+
+  for sentence in y:
+    if sentence[0] == 'C':
+      c_sentences+=1
+
+  print(f"t_sentences: {c_sentences}")
+  return c_sentences
+
 file_name = "content/DataSets.zip"
 
 with ZipFile(file_name, 'r') as zip:
@@ -66,10 +88,12 @@ with ZipFile(file_name, 'r') as zip:
   print('Done')
 
 #Creation of empty arrays
-id = [0] * 258
-label = [0] * 258
-T_avg_Length = [0] * 258
-C_avg_Length = [0] * 258
+id = [0] * 257
+label = [0] * 257
+T_avg_Length = [0] * 257
+C_avg_Length = [0] * 257
+T_turns_arr = [0] * 257
+C_turns_arr = [0] * 257
 
 count = 0
 #Opens csv file read filenames from file path may need to be changed
@@ -84,16 +108,18 @@ with open('DataSets/labeleddata/testlabels.csv', newline='') as csvfile:
       label[count] = row["label"]
       T_avg_Length[count] = w_countT(y)
       C_avg_Length[count] = w_countC(y)
+      T_turns_arr[count] = T_turns(y)
+      C_turns_arr[count] = C_turns(y)
       count = count + 1
 
 count = 0
 print(T_avg_Length)
 #Opens empty csv and writes labeled data
 with open('labeled.csv', 'w', newline='') as csvfile:
-    fieldnames = ['id', 'label', 'T_Words/Turn', 'C_Words/Turn']
+    fieldnames = ['id', 'label', 'T_Words/Turn', 'C_Words/Turn', 'T_Turns', 'C_Turns']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     count = 0
     writer.writeheader()
     for at in range(len(id)):
-      writer.writerow({'id': id[count], 'label': label[count], 'T_Words/Turn': T_avg_Length[count], 'C_Words/Turn': C_avg_Length[count]})
+      writer.writerow({'id': id[count], 'label': label[count], 'T_Words/Turn': T_avg_Length[count], 'C_Words/Turn': C_avg_Length[count], 'T_Turns': T_turns_arr[count], 'C_Turns': C_turns_arr[count]})
       count = count + 1
