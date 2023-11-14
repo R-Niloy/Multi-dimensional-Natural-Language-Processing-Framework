f7639821f7c840edb3dede4c2a52288d0f589ec0
sarmed <sarmed@Ubuntu-22.04.user.virtualbox.org>
2023-11-13 22:54:45 -0500

    Fully fixed and working Therapist and client words/turn added, next add total turns taken and statistical features as well as other features outlined in our previous report, NOTE sentiment analysis section was removed

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,52 +2,62 @@
 from zipfile import ZipFile
 import csv
 import pandas as pd
+#NOTe  TO SELF RESEARCH BASIC TRANSCIPT based CLASSIFCIATION MODELS FOR OTHER TOPICS FOR MORE FEATURE IDEAS.
+
 
 #Opens dataset reads data into array and removes therapist conversations
 def open_file(file_name):
   f = open(file_name, "r")
   x = f.readlines()
-  print(x)
   f.close
   for a in x:
     if(a[0] == 'T'):
       x.remove(a)
-  print(len(x) )
-  print(x)
   return x
 
-#Gets word count from array of strings
-def w_count(x):
+#Opens dataset reads data all data into array
+def open_file_allLines(file_name):
+  f = open(file_name, "r")
+  y = f.readlines()     #Y will have the whole transcript as an array
+  f.close
+  return y
+
+#Gets Average words/turn Therapist
+def w_countT(y):
   wcount = 0
 
   t_wcount = 0
-  c_wcount=0
-
   t_sentences = 0
-  c_sentences = 0
 
-  for sentence in x:
-    wcount = wcount + len(sentence.split()) - 1
-    if sentence[0] == 'C':
-      c_wcount= c_wcount + len(sentence.split()) - 1  
-      c_sentences+=1
-    else:
+  for sentence in y:
+    if sentence[0] == 'T':
       t_wcount= t_wcount + len(sentence.split()) - 1  
       t_sentences+=1
 
-
   print(f"t_sentences: {t_sentences}")
-  print(f"c_sentences: {c_sentences}")
-  print(f"x: {len(x)}")
 
-  wcount = wcount / len(x)
-  c_wcount = c_wcount / c_sentences
   t_wcount = t_wcount / t_sentences
 
-  print(wcount)
-  print(f"Client Words/Turn: {c_wcount}")
   print(f"Therapist Words/Turn: {t_wcount}")
-  return wcount
+  return t_wcount
+
+#Gets Average words/turn Client
+def w_countC(y):
+  c_wcount = 0
+  c_sentences = 0
+
+  for sentence in y:
+    if sentence[0] == 'C':
+      c_wcount= c_wcount + len(sentence.split()) - 1  
+      c_sentences+=1
+
+  print(f"c_sentences: {c_sentences}")
+
+  c_wcount = c_wcount / c_sentences
+
+  print(c_wcount)
+  print(f"Client Words/Turn: {c_wcount}")
+  return c_wcount
 
 file_name = "content/DataSets.zip"
 
@@ -58,9 +68,6 @@ with ZipFile(file_name, 'r') as zip:
 #Creation of empty arrays
 id = [0] * 258
 label = [0] * 258
-Sent = [0] * 258
-Length = [0] * 258
-
 T_avg_Length = [0] * 258
 C_avg_Length = [0] * 258
 
@@ -72,17 +79,21 @@ with open('DataSets/labeleddata/testlabels.csv', newline='') as csvfile:
       if(row["id"] == 'high_022' or row["id"] == 'low_023'):
         continue
       x = open_file("DataSets/"+row["id"])
+      y = open_file_allLines("DataSets/"+row["id"])
       id[count] = row["id"]
       label[count] = row["label"]
-      Length[count] = w_count(x)
+      T_avg_Length[count] = w_countT(y)
+      C_avg_Length[count] = w_countC(y)
       count = count + 1
 
+count = 0
+print(T_avg_Length)
 #Opens empty csv and writes labeled data
 with open('labeled.csv', 'w', newline='') as csvfile:
-    fieldnames = ['id', 'label','Sent','Length']
+    fieldnames = ['id', 'label', 'T_Words/Turn', 'C_Words/Turn']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     count = 0
     writer.writeheader()
     for at in range(len(id)):
-      writer.writerow({'id': id[count], 'label': label[count], 'Sent': Sent[count],'Length': Length[count]})
+      writer.writerow({'id': id[count], 'label': label[count], 'T_Words/Turn': T_avg_Length[count], 'C_Words/Turn': C_avg_Length[count]})
       count = count + 1
